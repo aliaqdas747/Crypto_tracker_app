d5ab37809766392df250c93352064beabfdc546b
aliaqdas747 <142832510+aliaqdas747@users.noreply.github.com>
2024-02-04 08:53:48 +0500

    market provider is added to manage Api data

---
+++ lib/Providers/marketProvider.dart
@@ -0,0 +1,38 @@
+import 'package:crypto_tracker/Models/Api.dart';
+import 'package:crypto_tracker/Models/CryptoCurrency.dart';
+import 'package:flutter/cupertino.dart';
+
+// A provider class for managing market-related data
+class  MarketProvider with ChangeNotifier{
+  // Boolean flag to indicate whether data is currently being loaded
+  bool isLoading = true;
+
+
+  // List to store CryptoCurrency objects representing market data
+  List<CryptoCurrency> markets = [];
+// Method to fetch market data asynchronously
+   void fetchData() async {
+     // Use the API class to get market data
+    List<dynamic> _markets = await API.getMarket();
+
+    // Temporary list to store CryptoCurrency objects
+    List<CryptoCurrency> temp = [];
+
+    // Loop through the retrieved market data and convert each entry to a CryptoCurrency object
+    for(var market in _markets){
+      // Create a new CryptoCurrency object from the JSON representation
+      CryptoCurrency newCrypto = CryptoCurrency.fromJSON(market);
+      // Add the new CryptoCurrency object to the temporary list
+      temp.add(newCrypto);
+    }
+
+    // Update the markets list with the converted CryptoCurrency objects
+    markets = temp;
+
+    // Set isLoading to false to indicate that data loading is complete
+    isLoading = false;
+
+    // Notify any listeners (typically UI components) that the data has been updated
+    notifyListeners();
+   }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:crypto_tracker/Models/CryptoCurrency.dart';
 import 'package:crypto_tracker/Pages/HomePage.dart';
+import 'package:crypto_tracker/Providers/marketProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 void main() {
@@ -11,18 +13,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider<MarketProvider>(
+        create: (context)=> MarketProvider(),)
+    ],
+    child:  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:  HomePage()
+        ),
+        home:  HomePage()
+    ),
     );
+
+
   }
 }
